da91394cfd6f7ec26fd59146c2418fc00f0348ab
kavimcFox <124341107+kavimcFox@users.noreply.github.com>
2023-03-14 21:26:43 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -30,33 +30,33 @@ global.anticall = true //anti call
 global.userRegister = false
 global.autoblok212 = false //ubah jadi true biar nmr 212 di blok sama bot
 global.autolevelup = true
-global.groupOnly = false //bot hanya bisa di gunakan dalam grup
+global.groupOnly = true //bot hanya bisa di gunakan dalam grup
 
 //language
 //Available in indonesia , english & spanyol 
 //You can request others language 
-global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
+global.language = english //change indonesia to english if you don't understand the language used by the bot
 //global.language = english // use this for English language
 //global.language = spanyol // use this for Spanyol language
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
+global.botname = "FoxAlphabot-Mdོ" //namabot kalian
+global.ownername = "ᴹᴿ᭄ FoxVPncོ ×፝֟͜×" //nama kalian
 global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
+global.youtube = "" //bebas asal jan hapus
 global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Indonesia" //bebas
-global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62821931157232"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
+global.email = "" //bebas
+global.region = "Srilanka" //bebas
+global.timezone = 'Asia/Colombo' //  timezone wib
+global.premium = ['761562672'] //premium user
+global.owner = ["0723903414", "0715168503"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "0723903414" // nomor wa kalian
 global.ownernomerr = "+"+ownernomer //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/edca4b513ddf29fd52c0e.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
+global.thumbGif = ''
 global.packname = '© ' + botname //sticker wm ubah
 global.author = 'Created By ' + ownername //sticker wm ganti nama kalian
 global.sessionName = 'session'
@@ -117,7 +117,7 @@ global.buttonDefault2 = [{
    }
 }, {
    quickReplyButton: {
-      displayText: '🎁 Donasi 🎁',
+      displayText: '🎁  🎁',
       id: '.donasi'
    }
 }]
